40ff66bf859ceb7a6643ffeffbd378c5267e4f35
Connor Shubham Verlekar <63520509+connorv001@users.noreply.github.com>
2021-03-18 17:01:51 +0530

    Update notebook_executor.sh

--- notebook_executor.sh
+++ notebook_executor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install -U papermill[all]
+python3 -m pip install papermill[all]
 readonly INPUT_NOTEBOOK_GCS_FILE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/input_notebook -H "Metadata-Flavor: Google")
 readonly OUTPUT_NOTEBOOK_GCS_FOLDER=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/output_notebook -H "Metadata-Flavor: Google")
 readonly PARAMETERS_GCS_FILE=$(curl --fail http://metadata.google.internal/computeMetadata/v1/instance/attributes/parameters_file -H "Metadata-Flavor: Google")
