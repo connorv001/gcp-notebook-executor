7d860c47d010bfc3618b69a12ce90a3b3d2e443b
Connor Shubham Verlekar <63520509+connorv001@users.noreply.github.com>
2021-03-18 16:55:27 +0530

    Update notebook_executor.sh

--- notebook_executor.sh
+++ notebook_executor.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-if lspci -vnn | grep NVIDIA > /dev/null 2>&1; then
-  # Nvidia card found, need to check if driver is up
-  if ! nvidia-smi > /dev/null 2>&1; then
-    echo "Installing driver"
-    /opt/deeplearning/install-driver.sh
-  fi
-fi
 
-if [[ ! -z $(command -v conda) ]]; then
   source /opt/anaconda3/bin/activate base
   sudo /opt/anaconda3/bin/pip install -U papermill==0.19.1
-fi
 
 readonly INPUT_NOTEBOOK_GCS_FILE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/input_notebook -H "Metadata-Flavor: Google")
 readonly OUTPUT_NOTEBOOK_GCS_FOLDER=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/output_notebook -H "Metadata-Flavor: Google")
